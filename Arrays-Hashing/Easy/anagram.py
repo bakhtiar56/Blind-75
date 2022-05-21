9bc1d9c32a17cfc024859845d2a461f79e25e710
bakhtiar56 <81223935+bakhtiar56@users.noreply.github.com>
2022-05-21 15:43:45 +0500

    Update anagram.py
    Given two strings s and t, return true if t is an anagram of s, and false otherwise.

--- Arrays-Hashing/Easy/anagram.py
+++ Arrays-Hashing/Easy/anagram.py
@@ -1,3 +1,22 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+
+Input: s = "rat", t = "car"
+Output: false
+'''
+
+
+
+
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s_count={}#count each letter in s
